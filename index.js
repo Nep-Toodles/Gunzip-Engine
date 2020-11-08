433785350845ba59ca549e1cc0841b4ecdecfb6a
Bibekz-dev <59413955+Bibekz-dev@users.noreply.github.com>
2020-11-08 16:02:32 +0545

    UpgradeUI and add a scriptor

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-window.onload=()=>{
-  RunThePreview = "Gravity"
-  $("scriptView").onclick=()=>{
-  $("#GameWindow").hide()
-}
+
+window.onload = function(){
+  document.getElementById("scriptingWindow").style.display = "none"; //By deafult it is hidden
+
+  //Events of Tab Window Start
+  document.getElementById("sceneView").onclick =()=>{
+    document.getElementById("scenesMenu").style.display = "block";
+    document.getElementById("scriptingWindow").style.display = "none";
+  }
+  document.getElementById("scriptView").onclick =()=>{
+    document.getElementById("scenesMenu").style.display = "none";
+    document.getElementById("scriptingWindow").style.display = "block";
+  }
+    //Events of Tab windows End
+  
 }
